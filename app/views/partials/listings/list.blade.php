f7df7a5fda3e7bec6c94d178780be7d391481093
jfoxcoder <jfoxcoder@gmail.com>
2014-08-14 18:49:30 +1200

    Home page price visual bug fixed.

--- app/views/partials/listings/list.blade.php
+++ app/views/partials/listings/list.blade.php
@@ -26,12 +26,17 @@
 
                 <footer>
                     @if(Auth::check())
-                    <span style="height: 30px" class="btn icon-star3 {{ $wish }}" title="{{ $title}}" data-address="{{ $listing->getStreetAddress() }}></span>
+                    <span style="height: 30px"
+                          class="btn icon-star3 {{ $wish }}"
+                          title="{{ $title  }}"
+                          data-address="{{ $listing->getStreetAddress() }}"></span>
+
                     @else
 
                     <a href="{{ URL::route('login') }}" style="height: 30px" class="btn icon-star3 wish-off" title="Sign-in to create a wishlist"></a>
                     @endif
 
+                    {{--  --}}
                     <span class="right listing-price">$ {{ $listing->getFormattedPrice() }}</span>
                 </footer>
             </article>
